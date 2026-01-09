af38e721b43446d89c8b1e16af554714cbcad536
Bilal-Trigui <bilaltrigui06@gmail.com>
2026-01-09 10:20:15 -0500

    installed android studio

---
+++ music_player_project/src-tauri/gen/android/app/src/main/java/com/bilal_t/music_player_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bilal_t.music_player_project
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
